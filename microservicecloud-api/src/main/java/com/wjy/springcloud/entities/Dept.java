b243af4a4dd536cc74a583eab4f5ec67096464fa
WJY <364075720@qq.com>
2018-09-23 11:35:58 +0800

    生产者完成

---
+++ microservicecloud-api/src/main/java/com/wjy/springcloud/entities/Dept.java
@@ -0,0 +1,20 @@
+package com.wjy.springcloud.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class Dept implements Serializable {
+    private Long deptno;
+    private String dname;
+    private String db_source;
+
+    public Dept(String dname) {
+        this.dname = dname;
+    }
+}
