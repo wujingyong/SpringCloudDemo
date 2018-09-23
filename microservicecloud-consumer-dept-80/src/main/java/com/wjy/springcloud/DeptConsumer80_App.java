2343823645f370200657f956318f337e901f6e10
WJY <364075720@qq.com>
2018-09-23 21:53:34 +0800

    ribbon负载均衡轮询模式搭建完成

--- microservicecloud-consumer-dept-80/src/main/java/com/wjy/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/wjy/springcloud/DeptConsumer80_App.java
@@ -2,10 +2,12 @@ package com.wjy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class DeptConsumer80_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80_App.class,args);
