1043b990e4555b631d69f0f4102642bf5b623ee9
WJY <364075720@qq.com>
2018-09-23 16:11:36 +0800

    eureka服务注册中心显示信息更改

--- microservicecloud-provider-dept-8001/src/main/java/com/wjy/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/wjy/springcloud/DeptProvider8001_App.java
@@ -2,10 +2,12 @@ package com.wjy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class,args);
--- microservicecloud-provider-dept-8001/src/main/java/com/wjy/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/wjy/springcloud/controller/DeptController.java
@@ -26,4 +26,7 @@ public class DeptController {
     public List<Dept> list() {
         return service.list();
     }
+
+   /* @GetMapping(value = "/info")
+    public */
 }
